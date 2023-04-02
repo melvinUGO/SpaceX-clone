628075bcca85eac2f7a9f26d2a573e235a4717a1
melvinUGO <ugochukwumelvin@gmail.com>
2023-04-02 15:26:02 +0100

    first commit

---
+++ scripts/nav.js
@@ -0,0 +1,14 @@
+let isActive = false;
+function toggle() {
+  if (isActive) {
+    document.querySelector(".hamburger").className = "hamburger ";
+    document.querySelector(".shadow").className = "shadow ";
+    document.querySelector(".mobile-nav").className = "mobile-nav ";
+    isActive = false;
+  } else {
+    document.querySelector(".hamburger").className = "hamburger active";
+    document.querySelector(".shadow").className = "shadow active";
+    document.querySelector(".mobile-nav").className = "mobile-nav active";
+    isActive = true;
+  }
+}
